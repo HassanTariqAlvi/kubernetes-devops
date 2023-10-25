2d54790806e2896439cef3cc2ebfca80f7eaa284
Muhammad Hassan Tariq <hassan.tariq@oztechwork.com>
2023-10-25 17:39:30 +0500

    made changes in interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import kubeLogo from "/kubernetes.png";
-import viteLogo from "/vite.svg";
 import myGif from '/devops.gif';
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-	const bgColor = import.meta.env.VITE_COLOR;
+	
+	const bgColor = import.meta.env.VITE_COLOR || 'white';
 	
 
 	useEffect(() => {
@@ -18,31 +17,29 @@ function App() {
 
 	return (
 		<div>
-			{/* Add your name at the top left corner */}
-			<div className="app-name">Kubernetes Application
-			<div>Color: {bgColor}</div>
+			<div  className="app-name"> App-Color: 
+				<span className="app-name-light"> {bgColor} </span>
 			</div>
-		
-			
-
-			<div>
-				<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
-					<img src={myGif} className="logo" alt="DevOps logo" />
-				</a>
-				<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
-					<img
-						src={kubeLogo}
-						className="logo react"
-						alt="kube logo"
-					/>
-				</a>
+	
+			<div className="card">
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+					<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
+						<img src={myGif} className="logo" alt="DevOps logo" />
+					</a>
+					<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
+						<img src={kubeLogo} className="logo react" alt="kube logo" />
+					</a>
+				</div>
+	
+				<div className="my-name">
+					Developed by: <a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noopener noreferrer">
+						<span className="my-name-bold">Muhammad Hassan Tariq</span>
+					</a>
+				</div>
 			</div>
-			<div className="my-name">
- 				 Developed by: <a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noopener noreferrer"><span className="my-name-bold">Muhammad Hassan Tariq</span></a>
-			</div>
-
 		</div>
 	);
+	
 }
 
 export default App;
