2423bbed4e61a5f8235402b24b044bfdefcdd1ee
Muhammad Hassan Tariq <hassan.tariq@oztechwork.com>
2023-10-25 17:56:07 +0500

    made changes in interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,79 @@
 import { useState, useEffect } from "react";
 import kubeLogo from "/kubernetes.png";
-import myGif from '/devops.gif';
+import myGif from "/devops.gif";
 import "./App.css";
 
 function App() {
-	
-	const bgColor = import.meta.env.VITE_COLOR || 'white';
-	
+  const bgColor = import.meta.env.VITE_COLOR || "white";
 
-	useEffect(() => {
-		document.body.style.backgroundColor = bgColor;
-		return () => {
-			document.body.style.backgroundColor = null; // Reset to the original style on unmount
-		};
-	}, [bgColor]);
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+    return () => {
+      document.body.style.backgroundColor = null; // Reset to the original style on unmount
+    };
+  }, [bgColor]);
 
-	return (
-		<div>
-			<div  className="app-name"> App-Color: 
-				<span className="app-name-light"> {bgColor} </span>
-			</div>
-	
-			<div className="card">
-				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-					<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
-						<img src={myGif} className="logo" alt="DevOps logo" />
-					</a>
-					<a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noreferrer">
-						<img src={kubeLogo} className="logo react" alt="kube logo" />
-					</a>
-				</div>
-	
-				<div className="my-name">
-					Developed by: <a href="https://www.linkedin.com/in/muhammad-hassan-tariq/" target="_blank" rel="noopener noreferrer">
-						<span className="my-name-bold">Muhammad Hassan Tariq</span>
-					</a>
-				</div>
-			</div>
-		</div>
-	);
-	
+  return (
+    <div>
+      <div className="app-container">
+        <div className="app-name">
+          App-Color
+          <span className="app-name-light"> {bgColor}</span>
+        </div>
+        <div className="app-name">
+          App Name
+          <span className="app-name-light"> K8s Practice</span>
+        </div>
+        <div className="app-name">
+          Version
+          <span className="app-name-light"> v1.0</span>
+        </div>
+      </div>
+
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <a
+            href="https://www.linkedin.com/in/muhammad-hassan-tariq/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={myGif} className="logo" alt="DevOps logo" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/muhammad-hassan-tariq/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={kubeLogo} className="logo react" alt="kube logo" />
+          </a>
+        </div>
+
+        <div className="my-name">
+          Developed by:{" "}
+          <a
+            href="https://www.linkedin.com/in/muhammad-hassan-tariq/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="my-name-bold">Muhammad Hassan Tariq</span>
+          </a>
+        </div>
+      </div>
+      <div>
+        {/* Add the line at the bottom */}
+        <p className="info-text">
+          App color <span className="my-name-bold">'{bgColor}'</span> is being
+          printed from .env
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
