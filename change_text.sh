99ae1fe56da05cc37503edb5150e4fe538b516a1
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-10-30 17:39:59 +0500

    Update change_text.sh

--- change_text.sh
+++ change_text.sh
@@ -4,7 +4,7 @@
 html_file="/var/www/k8s-devops/index.html"
 
 # Replace "k8s" with "k888s" in the HTML file
-sed -i 's/k8s/k888s/g' "$html_file"
+sed -i 's/k8s/k8uus/g' "$html_file"
 
 # Exit with a success status
 exit 0
